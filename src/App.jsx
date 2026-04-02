4700032b94c04872e487cdacba6c03865fb1f86d
zypherson <taylorzypherson@gmail.com>
2026-04-02 11:49:50 -0400

    set up state

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,32 @@
-
+/* eslint-disable no-unused-vars */
+import { useState } from "react";
 
 function App() {
-  return <h1>Kanban Board</h1>;
+  const [tasks, setTasks] = useState({
+    todo: [],
+    inProgress: [],
+    done: [],
+  });
+
+  return (
+    <div className="app">
+      <h1>Kanban Board</h1>
+
+      <div className="board">
+        <div className="column">
+          <h2>To Do</h2>
+        </div>
+
+        <div className="column">
+          <h2>In Progress</h2>
+        </div>
+
+        <div className="column">
+          <h2>Done</h2>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default App;
